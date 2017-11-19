e765137ea40567f3477b73462dc4c0267a32c72c
Anubhav Singh <singh.anubhav1772@gmail.com>
2017-11-19 20:42:48 +0530

    Apriori Algorithm

--- Apriori/src/com/major/apriori/Apriori.java
+++ Apriori/src/com/major/apriori/Apriori.java
@@ -1,6 +1,14 @@
 package com.major.apriori;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.major.tuple.Tuple;
@@ -10,6 +18,7 @@ public class Apriori
     Set<Tuple> C;
     Set<Tuple> L;
     int min_support;
+    int dataset[][];
     
     public Apriori()
     {
@@ -42,6 +51,80 @@ public class Apriori
     	}
     	
     }
+    
+    public void readDataset_csv()throws IOException
+    {
+    	String csv="/home/anubhav55182/eclipse-workspace/Apriori/apriori.csv";
+    	BufferedReader br=null;
+    	String line="";
+    	Map<Integer,List<Integer>> map;       // <Transaction_id,List of Itemsets>
+    	List<Integer> temp;
+    	try
+    	{
+    		br=new BufferedReader(new FileReader(csv));
+    		map=new HashMap<Integer,List<Integer>>();
+    		//line=br.readLine();
+    		String transaction[];             // consists of purchased items id in a particular transaction
+    		int i=1;
+    		while((line=br.readLine())!=null)
+    		{
+    			transaction=line.split(",");
+    			Integer id;                    // items id
+    			temp=new LinkedList<Integer>();
+    			for(String s:transaction)
+    			{
+    				id=Integer.parseInt(s);
+    				temp.add(id);
+    			}
+    			map.put(i,temp);
+    			++i;
+    			
+    		}
+    		
+    		Set<Integer> keyset=map.keySet();  // get the keys in map
+    		dataset=new int[keyset.size()][];
+    		Iterator<Integer> it=keyset.iterator();
+    		int count=0;
+    		while(it.hasNext())
+    		{
+    			temp=map.get(it.next());       // getting itemsets of each transaction
+    			Integer items[]=temp.toArray(new Integer[0]);
+    			dataset[count]=new int[items.length];
+    			for(int j=0;j<items.length;j++)
+    			{
+    				dataset[count][j]=items[j].intValue();
+    			}
+    			++count;
+    		}
+    		
+    		
+    		
+    	}
+    	catch(FileNotFoundException fnfe)
+    	{
+    		fnfe.printStackTrace();
+    	}
+    	catch(IOException ioe)
+    	{
+    		ioe.printStackTrace();
+    	}
+    	finally
+    	{
+    		if(br!=null)
+    		{
+    			try
+    			{
+    				br.close();
+    			}
+    			catch(IOException e)
+    			{
+    				e.printStackTrace();
+    			}
+    		}
+    	}
+    }
+    
+    
     
 	public static void main(String[] args) 
 	{
